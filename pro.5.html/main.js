8f26d1c152db442b438bcdadcb3845ca64ecaa02
Agam789 <93301113+Agam789@users.noreply.github.com>
2022-11-25 23:12:45 +0530

    Website Is Mainly Using Html And Css And Bootsrap
    Very Easy To Use Code In Your Daily Project

---
+++ pro.5.html/main.js
@@ -0,0 +1,9 @@
+//active Navbar
+document.addEventListener("DOMContentLoaded",() =>{
+    function counter (id,start,end,duration){
+        let obj =document.getElementById(id)
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1
+    }
+});
